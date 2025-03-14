9be516e9ad3f5fb9b7078827c92082d9a7243ad5
𝐒𝐇𝐄𝐈𝐊𝐇 𝐀𝐋𝐈 𝐀𝐇𝐌𝐀𝐃 <aliahmad241224@gmail.com>
2025-03-14 06:07:30 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: '𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ ♡', // Your name
+  ownerNumber: '923133566222', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
